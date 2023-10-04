644eeccd7ee20c4b4e25fc47c2041bf30b1a6b39
Martin Burga <martin.burga@udla.edu.ec>
2023-10-04 16:22:03 -0500

    Se hace excepciones para el numero 1 y 0

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 int numero;
+int primo;
 int main() {
     
     printf("Bienvenido a la calculadora de numeros primos\n");
     printf("Inserte un numero entero");
+    scanf ("%d", &numero);
+    
+    if (numero=1 || numero=0)
+    {
+        printf("El numero 1 y 0 no son numeros primos");
+    }
+    
 
     return 0;
 }
